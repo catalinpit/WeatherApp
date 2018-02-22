3e3e6d51fbc5b697ef5b098303243b5993d873c7
Catalin Pit <catalinpit@gmail.com>
2018-02-22 13:19:30 +0000

    modified index.js from home/routes to add title and a message

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -8,11 +8,11 @@ export default class Home extends Component {
 	render() {
 		return (
 			<div class={style.home}>
-				<h1>Home route</h1>
+				<h1>Group Project GUI</h1>
 				<Card>
 					<Card.Primary>
 						<Card.Title>Home card</Card.Title>
-						<Card.Subtitle>Welcome to home route</Card.Subtitle>
+						<Card.Subtitle>Welcome to our unfinished weather application!</Card.Subtitle>
 					</Card.Primary>
 					<Card.SupportingText>
 						Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
