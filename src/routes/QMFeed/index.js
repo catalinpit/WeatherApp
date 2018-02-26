5fe8a86186e28872bea8d5f077c47cd70a0a3e0a
PrinceOfTheEast <36744655+PrinceOfTheEast@users.noreply.github.com>
2018-02-26 18:55:07 +0000

    testing2

--- src/routes/QMFeed/index.js
+++ src/routes/QMFeed/index.js
@@ -8,14 +8,16 @@ export default class QMFeed extends Component {
 	render() {
 		return (
 			<div class={style.QMFeed}>
-				<h1>Group Project GUI</h1>
+				<h1>QM News</h1>
+
 				<Card>
 					<Card.Primary>
-						<Card.Title>Home card</Card.Title>
+						<Card.Title>Home card</Card.Title>0
 						<Card.Subtitle>Welcome to our22 unfinished weather application!</Card.Subtitle>
 					</Card.Primary>
 					<Card.SupportingText>
 						Shah was here
+						Now his not
 					</Card.SupportingText>
 					<Card.Actions>
 						<Card.Action>OKAY</Card.Action>
