d94bd85a43dafd9adf598ca5e89ff6233114acef
PrinceOfTheEast <36744655+PrinceOfTheEast@users.noreply.github.com>
2018-02-27 12:28:25 +0000

    RSS

--- src/routes/QMFeed/index.js
+++ src/routes/QMFeed/index.js
@@ -8,8 +8,20 @@ export default class QMFeed extends Component {
 	render() {
 		return (
 			<div class={style.QMFeed}>
-				<h1>QM News</h1>
+				<div id="header">
+					<h1>QM News</h1>
+				</div>
+				<table border="4">
+				<tr>
+					<td>
+						hello this is a box
+					</td>
+				</tr>
+				</table>
+				<iframe width="250" height="450" class="rssdog" src="http://www.rssdog.com/index.htm?url=http%3A%2F%2Ffeeds.bbci.co.uk%2Fnews%2Frss.xml&mode=html&showonly=&maxitems=10&showdescs=1&desctrim=0&descmax=0&tabwidth=100%25&linktarget=_blank&textsize=inherit&bordercol=%23d4d0c8&headbgcol=%23999999&headtxtcol=%23ffffff&titlebgcol=%23f1eded&titletxtcol=%23000000&itembgcol=%23ffffff&itemtxtcol=%23000000&ctl=0"></iframe>
 
+
+				/***
 				<Card>
 					<Card.Primary>
 						<Card.Title>Home card</Card.Title>0
@@ -22,7 +34,7 @@ export default class QMFeed extends Component {
 					<Card.Actions>
 						<Card.Action>OKAY</Card.Action>
 					</Card.Actions>
-				</Card>
+				</Card>*//////
 			</div>
 		);
 	}
