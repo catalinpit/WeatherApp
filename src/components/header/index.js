68ab087043da0939d1efb712b5d86b35ac44f670
Catalin Pit <catalinpit@gmail.com>
2018-02-27 15:16:49 +0000

    added jquery

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -60,7 +60,7 @@ export default class Header extends Component {
 							<Toolbar.Icon menu onClick={this.openDrawer}>
 								menu
 							</Toolbar.Icon>
-							<Toolbar.Title>Preact app</Toolbar.Title>
+							<Toolbar.Title>QMUL Weather</Toolbar.Title>
 						</Toolbar.Section>
 						<Toolbar.Section align-end onClick={this.openSettings}>
 							<Toolbar.Icon>settings</Toolbar.Icon>
