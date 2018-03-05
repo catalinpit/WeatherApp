870037cd06cc60cbebce984e7984e90438c39598
Anees Imran <aneesimran@outlook.com>
2018-03-05 14:31:03 +0000

    changed kmh to mph for weather speed

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -67,7 +67,7 @@ export default class Home extends Component {
 					<Card.Primary>
 
 						<div class = {style.realFeel}><p>Real feel: {this.state.feelslike} °C</p></div>
-						<div class = {style.windSpeed}><p>Wind speed: {this.state.windspeed} km/h</p></div>
+						<div class = {style.windSpeed}><p>Wind speed: {this.state.windspeed} mph</p></div>
 						<div class = {style.currentTemp}><p>{this.state.temp} °C</p></div>
 						<div class = {style.cweatherImg}><p> <img src={this.state.weatherimg} /></p></div>
 						<div class = {style.weatherInfo}><p>{this.state.weather}</p></div>
